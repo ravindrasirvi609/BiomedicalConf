47a50b7a6f8392296db0517bbfb4ccf4e2900016
Ravindra <sirviravindra609@gmail.com>
2024-11-23 09:46:43 +0530

    feat: Add configuration files and global styles
    - Added .eslintrc.json for linting configuration
    - Added next.config.ts for Next.js configuration
    - Added postcss.config.mjs for PostCSS configuration
    - Added tailwind.config.ts for Tailwind CSS configuration
    - Added .gitignore to ignore certain files and directories
    - Added tsconfig.json for TypeScript configuration
    - Added package.json with project dependencies
    - Added globals.css for global styles

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import ConferenceHomepage from "@/components/ConferenceHomepage";
+
+export default function Home() {
+  return (
+    <div>
+      <ConferenceHomepage />
+    </div>
+  );
+}
+
+//cdb4db-ffc8dd-ffafcc-bde0fe-a2d2ff
